ce1e89926e7069f384690053e0eab2174c1c6aa2
damongsanga <damongsanga.mike0413lata@gmial.com>
2023-08-11 15:39:35 +0900

    운동영상리뷰

--- project/jwonsrc/reveiw.js
+++ project/jwonsrc/reveiw.js
@@ -5,5 +5,7 @@ let videoHtml = "";
 console.log(data);
 let dataJson = JSON.stringify(data);
 let videoList = JSON.parse(dataJson);
+let video = videoList[0];
+let mainVideo = document.querySelector(".main-video");
 
-document.querySelector
+mainVideo.innerHTML = `<iframe width="1271" height="715" src="https://www.youtube.com/embed/${video.id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
